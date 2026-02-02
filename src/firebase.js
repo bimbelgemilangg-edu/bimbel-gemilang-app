5b8fe13c7be4ac7915cd07d19538b4ccca2bc5cf
bimbelgemilangg-edu <bimbelgemilangg@gmail.com>
2026-02-02 11:45:22 +0700

    bismillah

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
-// FILE: src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // 1. Wajib import Auth
 
+// Konfigurasi asli Anda
 const firebaseConfig = {
   apiKey: "AIzaSyCpwCjxcKwVKd0qBnezgRPV2MuZe1avVvQ",
   authDomain: "gemilangsystem.firebaseapp.com",
@@ -11,5 +12,12 @@ const firebaseConfig = {
   appId: "1:1078433073773:web:cdb13ae553efbc1d1bcd64"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+
+// Initialize Services
+const db = getFirestore(app);
+const auth = getAuth(app); // 2. Nyalakan mesin Login
+
+// 3. Export semuanya agar bisa dipakai di file lain
+export { db, auth, firebaseConfig };
